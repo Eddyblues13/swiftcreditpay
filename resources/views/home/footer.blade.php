a4bccc04835f839a8d2d62008150ae9aac19d7b5
blueswayne133 <blueswayne133@gmail.com>
2025-06-21 17:57:56 -0700

    new changes

--- resources/views/home/footer.blade.php
+++ resources/views/home/footer.blade.php
@@ -93,6 +93,20 @@
 <script src="{{ asset('contents/synline/assets/js/main.js') }}"></script>
 <script src="{{ asset('contents/synline/assets/js/birthday.js') }}"></script>
 
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/68575495f4cfc5190e97ef6d/1iuai4r0e';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+
 </body>
 
 </html>
